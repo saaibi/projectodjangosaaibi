ba3c8afa79b514228ccf43d0b782edad2d8c14b0
saaibi <saaibiyesid@hotmail.com>
2016-02-26 08:50:37 -0500

    proyecto

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hola Mundo")
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
 	return HttpResponse("Usted se encuentra en la pregunta %s." % question_id)
 
 
-def results(request, question_id):	
-    response = "se encuentra en los resultados de la pregunta %s. "
-    return HttpResponse(response % question_id)   
+def results(request, question_id):
+	response = "Se encuenra en los resultado de la pregunta %s. "
+	return HttpResponse(response % question_id)
 
 
 def vote(request, question_id):
-    return HttpResponse("Usted esta votamdo en la pregunta %s." % question_id)    
+	return HttpResponse("Usted esta votando en la pregunta %s." % question_id)
